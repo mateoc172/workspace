d634b2d44ed4089ff2026f65155318eaa6156838
mateoc172 <mateocorreav172@gmail.com>
2026-03-20 21:03:08 +0000

    ejercicios js

---
+++ 20-03/ejercicio1-3.js
@@ -0,0 +1,10 @@
+let variable = 10;
+console.log("valor inicial:");
+console.log(variable);
+variable = 20;
+console.log("cambio 1:");
+console.log(variable);
+variable = 30;
+console.log("cambio 2:");
+console.log(variable);
+const constante = 50;
---
+++ 20-03/modulo1.js
@@ -0,0 +1,9 @@
+var nombre="mateo";
+let edad=16;
+const ciudad="medellin";
+console.log("nombre:");
+console.log(nombre);
+console.log("la edad es:");
+console.log(edad);
+console.log("ciudad: ",ciudad);
+console.log("nombre: ",nombre, "edad: ",edad, "ciudad: ",ciudad);
